4dbf94352572a719259553f15f31b55c050f48cb
MinyoungJun <minyoung9353@gmail.com>
2014-09-15 02:18:54 +0900

    publisher and advertiser model created

---
+++ app/models/advertiser.rb
@@ -0,0 +1,4 @@
+class Advertiser < ActiveRecord::Base
+  belongs_to :user
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909125441) do
+ActiveRecord::Schema.define(version: 20140914073249) do
+
+  create_table "advertisers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "publishers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
