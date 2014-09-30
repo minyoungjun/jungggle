62bb74802552d745a007ab496dbe460826ab7ce8
MinyoungJun <minyoung9353@gmail.com>
2014-09-30 21:32:43 +0900

    active admin

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'autoprefixer-rails'
 #User 관련 devise
 gem 'devise'
 
+#active admin
+gem 'activeadmin', github: 'activeadmin'
+gem 'kaminari'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929010150) do
+ActiveRecord::Schema.define(version: 20140930114923) do
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "biddings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "country_id"
+    t.integer  "marketingtype_id"
+    t.integer  "company_id"
+    t.string   "name"
+    t.string   "service_url"
+    t.text     "detail"
+    t.integer  "budget"
+    t.datetime "deadline"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "billings", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "date"
+    t.string   "detail"
+    t.string   "payment_method"
+    t.integer  "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "carts", force: true do |t|
     t.integer  "user_id"
@@ -40,6 +97,24 @@ ActiveRecord::Schema.define(version: 20140929010150) do
 
   create_table "countries", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoices", force: true do |t|
+    t.integer  "billing_id"
+    t.integer  "invoice_number"
+    t.string   "receiver"
+    t.string   "email"
+    t.text     "description"
+    t.string   "price"
+    t.string   "payment_method"
+    t.string   "order_number"
+    t.string   "bank_name"
+    t.string   "bank_swift_code"
+    t.text     "bank_addredd"
+    t.string   "beneficiary_name"
+    t.string   "iban"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
