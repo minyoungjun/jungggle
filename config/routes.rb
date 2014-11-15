c4247719e25abae0031549a0b2e2db99459963b1
designicca <ssyoung6754@gmail.com>
2014-11-15 12:27:31 +0900

    email-confirn페이지 생성 및 퍼블리싱

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
 
   get 'publishers/createblack'
   get 'users/finish_signup2'
+  get 'users/email_confirm'
   resources "home" do
     get :autocomplete_company_name, :on => :collection
   end
