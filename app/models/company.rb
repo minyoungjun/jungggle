2af01eaffa90db7e393245c1ef16753d1858912d
MinyoungJun <minyoung9353@gmail.com>
2014-11-19 03:47:39 +0900

    detail

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,4 +13,24 @@ class Company < ActiveRecord::Base
   has_many :products
   has_many :comlangs
   has_many  :comclients
+
+  def employee_count
+    count = String.new
+    case self.num_employee
+    when 0
+      count = "1~10"
+    when 1
+      count = "10~50"
+    when 2
+      count = "50~100"
+    when 3
+      count = "100~500"
+    when 4
+      count = "500~1000"
+    when 5
+      count = "1000+"
+    end
+    return count
+  end
+
 end
