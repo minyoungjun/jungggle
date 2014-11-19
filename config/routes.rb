64918a2454b554b0df34cace0857cd37be1623d8
MinyoungJun <minyoung9353@gmail.com>
2014-11-19 22:50:57 +0900

    main

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   post 'advertisers/new_process'
   get "/products/attachment/:id" => "products#attachment"
 
+  post "products/search"
 
   devise_for :users, :controllers => {:registrations => "registrations" ,:sessions => "sessions" , :passwords => "passwords", :omniauth_callbacks => "omniauth_callbacks"}
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
