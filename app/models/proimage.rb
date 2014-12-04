49eb04cf4ff7bafd9ab4399b5f206109c75daa04
MinyoungJun <minyoung9353@gmail.com>
2014-12-05 01:21:25 +0900

    large

--- app/models/proimage.rb
+++ app/models/proimage.rb
@@ -1,5 +1,5 @@
 class Proimage < ActiveRecord::Base
-    has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    has_attached_file :photo, :styles => {:large => "1000X1000>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :detail
